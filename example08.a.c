758c5ac6b3bcdfc726a2e0bedf1e8274ae255b68
BandarALThobaiti <bndoooory511@gmail.com>
2026-07-07 15:02:55 +0300

    change the value

--- example08.a.c
+++ example08.a.c
@@ -1,4 +1,4 @@
-for(int x = 0 ; x < 10 ; x++){
+for(int x = 0 ; x < 11 ; x++){
     printf("X is: %d\n ", x); // print current value of x 
     #ifdef both // include extra code only if "out" 
     #include"example08.b.c"
